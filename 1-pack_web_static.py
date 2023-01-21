cfb1350f7ce9efdb420e2056b373dd0712b48ae0
scaarifintl <scaarifintl@gmail.com>
2023-01-21 16:15:19 +0700

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,22 +2,28 @@
 """ Generates a .tgz archive from the contents of the web_static folder
     in AirBnB Clone repo
 """
+import os
 from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
     """ creates the versions folder and it,
         generates the archive
     """
-    # create the directory versions if it doesn't exist
-    local('mkdir -p versions')
+    # create the directory 'versions' if it doesn't exist
+    if not os.path.isdir('versions'):
+        if local('mkdir -p versions').failed:
+            return None
     # generate the archive into the created folder
-    archive = local('tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-            web_static/* web_static')
-    if archive.succeeded:
-        # get the newly created file and create a path to return
-        res = local('ls -1t versions/', capture=True)
-        archive_file = res.split('\n')[0]
-        return f'versions/{archive_file}'
-    else:
+    now = datetime.now()
+    now_fstr = now.strftime("%Y%m%d%H%M%S")
+    sources = 'web_static/* web_static'
+    archive_path = f'versions/web_static_{now_fstr}.tgz'
+    print(f'Packing web_static to {archive_path}')
+    if local(f'tar -cvzf {archive_path} {sources}').failed:
         return None
+    # get the newly created file and create a path to return
+    size = os.path.getsize(archive_path)
+    print(f'web_static packed: {archive_path} -> {size}')
+    return archive_path
