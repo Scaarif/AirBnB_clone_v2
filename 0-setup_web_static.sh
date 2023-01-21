9826e93eeae28c7a28ddc25ed4f7890a20afeb6e
scaarifintl <scaarifintl@gmail.com>
2023-01-21 14:22:00 +0700

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,23 +9,18 @@
 #	update the Nginx configuration to server the content of /data/web_sttaic/current/ to 'hbnb_static' eg
 # (ex. https://mydomainname.tech/hbnb_static) use alias inside Nginx configuration...
 #	Resttart Nginx after updating the config... 
-
-# check that nginx is installed
-installed=$(sudo apt-mark showinstall | grep "nginx" &> /dev/null; echo $?)
-if [ "$installed" -ne 0 ]
-then
-	sudo apt-get -y update
-	sudo apt-get -y install nginx
-fi
+	
+apt-get -y update
+apt-get -y install nginx
 # create the folders needed (/data/ folders)
-mkdir -p ~/data/web_static/releases/test/
-mkdir -p ~/data/web_static/shared/
+mkdir -p data/web_static/releases/test/
+mkdir -p data/web_static/shared/
 # create a HTML file with simple content to test configuration
-echo "Hello Rahab!" > data/web_static/releases/test/index.html
+echo "<html><head></head><body><h2>Hello Rahab!</h2></body></html>" > data/web_static/releases/test/index.html
 # create a symbolic link (/data/web_static/current) to /data/web_static/releases/test/ (replace it if it alraedy exists)
-ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
+ln -sf data/web_static/releases/test/ data/web_static/current
 # give ownership of the /data/folder and its contents to 'ubuntu' owner and group
-sudo chown -R ubuntu:ubuntu ~/data/
+sudo chown -R ubuntu:ubuntu data/
 # configure Nginx to serve the contents of /data/web_static/current to 'hbnb_static' (eg https://mydomain.tech/hbnb_static ) . use ALIAS inside the configuration
 sudo sed -i '48i\ \tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 # restart nginx after updating the configuration
