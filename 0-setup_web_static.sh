09e38415be5f26c72170ab4e31fa1296367447e6
scaarifintl <scaarifintl@gmail.com>
2023-01-21 14:56:36 +0700

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 apt-get -y update
 apt-get -y install nginx
 service nginx start
-mkdir -p data/web_static/releases/test/
-mkdir -p data/web_static/shared/
-echo "Holberton School" > data/web_static/releases/test/index.html
-ln -sf data/web_static/releases/test/ data/web_static/current
-chown -R ubuntu:ubuntu data/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 sed -i '48i\\tlocation /hbnb_static {\n\t\t alias data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
