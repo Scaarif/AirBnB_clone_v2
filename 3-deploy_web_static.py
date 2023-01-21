01c3bc90d88afbccbfe462dfe95a2d97b6d03894
scaarifintl <scaarifintl@gmail.com>
2023-01-21 19:49:25 +0700

    task3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,32 +6,7 @@
 import os
 from fabric.api import *
 do_pack = __import__('1-pack_web_static').do_pack
-
-
-env.user = 'ubuntu'
-env.hosts = ['54.82.173.163', '18.210.20.118']
-
-
-def do_deploy(archive_path):
-    """ distribute an archive to all web servers """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        filename = archive_path[archive_path.find('/') + 1:]
-        folder = filename[:filename.find('.')]
-        put(archive_path, "/tmp/")
-        run(f'mkdir -p data/web_static/releases/{folder}/')
-        run(f'tar -xzf /tmp/{filename} -C data/web_static/releases/{folder}/')
-        with cd(f"data/web_static/releases/{folder}/"):
-            run('mv web_static/* .; rm -rf web_static')
-        run(f'rm -f /tmp/{filename}')
-        run('rm -f data/web_static/current')
-        run(f'ln -s data/web_static/releases/{folder}/ \
-                data/web_static/current')
-        print('New version deployed!')
-        return True
-    except Exception:
-        return False
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 def deploy():
