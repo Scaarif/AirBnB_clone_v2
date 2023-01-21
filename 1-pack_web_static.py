ddd7ebabb6641c258f4e1b397982fc04a9883690
scaarifintl <scaarifintl@gmail.com>
2023-01-21 16:38:35 +0700

    ---

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder."""
-from fabric.api import local
-import time
+""" Generates a .tgz archive from the contents of the web_static folder
+    in AirBnB Clone repo
+"""
+import os
+from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
-    """Generate an tgz archive from web_static folder"""
+    """ creates the versions folder and it,
+        generates the archive
+    """
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
+        # create the directory 'versions' if it doesn't exist
+        local('mkdir -p versions')
+        # generate the archive into the created folder
+        now = datetime.now()
+        now_fstr = now.strftime("%Y%m%d%H%M%S")
+        archive_path = f'versions/web_static_{now_fstr}.tgz'
+        # print(f'Packing web_static to {archive_path}')
+        if local(f'tar -cvzf {archive_path} web_static').failed:
+            return None
+        # get the newly created file and create a path to return
+        size = os.path.getsize(archive_path)
+        # print(f'web_static packed: {archive_path} -> {size}Bytes')
+        return archive_path
+    except Exception:
         return None
