fdac63bb0793930686ff9f314c4466f92b4293de
scaarifintl <scaarifintl@gmail.com>
2023-01-21 14:49:12 +0700

    ---

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# Configure Nginx servers, preparing them for web_static deployment	
-sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p data/web_static/releases/test/
-mkdir -p data/web_static/shared/
-echo "Holberton School" > data/web_static/releases/test/index.html
-ln -sf data/web_static/releases/test/ data/web_static/current
-sudo chown -R ubuntu:ubuntu data/
-sudo sed -i '48i\ \tlocation /hbnb_static {\n\t\t alias data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+# Configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
 exit 0
-
-
