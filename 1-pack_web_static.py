1a9e15dbd0ad94b1af48317ada5790d25d8641c1
scaarifintl <scaarifintl@gmail.com>
2023-01-21 16:29:06 +0700

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -20,10 +20,10 @@ def do_pack():
     now_fstr = now.strftime("%Y%m%d%H%M%S")
     sources = 'web_static'
     archive_path = f'versions/web_static_{now_fstr}.tgz'
-    print(f'Packing web_static to {archive_path}')
+    # print(f'Packing web_static to {archive_path}')
     if local(f'tar -cvzf {archive_path} {sources}').failed:
         return None
     # get the newly created file and create a path to return
     size = os.path.getsize(archive_path)
-    print(f'web_static packed: {archive_path} -> {size}Bytes')
+    # print(f'web_static packed: {archive_path} -> {size}Bytes')
     return archive_path
