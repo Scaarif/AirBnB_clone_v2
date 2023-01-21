be679bb9ad6894a6f8ec33903295edd3d02f95f8
scaarifintl <scaarifintl@gmail.com>
2023-01-21 17:19:11 +0700

    ---

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
-""" Based on the module '1-pack_web_static', distributes an archive
-    to your web servers (using the function it defines, do_deploy
-"""
+""" Function that compress a folder """
+from datetime import datetime
 from fabric.api import *
+import shlex
 import os
 
 
-# set the user to use for ssh
-env.user = 'ubuntu'
-# set the hosts/servers to be involved
 env.hosts = ['54.82.173.163', '18.210.20.118']
-
-# the fn wil then be run on both hosts (one after the other)
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ distributes an archive (in archive_path)
-    to all web_servers
-    Note: returns False if archive doesn't exist
-    """
-    # check that archive (archive_path) exists
-    filename = archive_path[archive_path.find('/') + 1:]
+    """ Deploys """
     if not os.path.exists(archive_path):
         return False
     try:
-        # upload the archive to the /tmp/ directory of the web server
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        folder = filename[:filename.find('.')]
-        # create the folder (to hold the archive)
-        run('mkdir -p /data/web_static/releases/{}/'.format(folder))
-        # extract the files/archive in created folder
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(filename, folder))
-        with cd(f"/data/web_static/releases/{}/".format(folder)):
-            run('mv web_static/* .; rm -rf web_static')
-        # delete the archive from the web server
-        run('rm -f /tmp/{}'.format(filename))
-        # delete the symbolic link /data/web_static/current on the web server
-        run('rm -f /data/web_static/current')
-        # create a new symbolic link /data/web_static/current
-        with cd("/data/web_static/releases/"):
-            run('ln -s {}/ /current'.fomat(folder))
-        # return True if all operations are correctly done, else return False
-        print('New version deployed!')
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
