395dbeb9034870702a25763ea17e38ff01898d6f
scaarifintl <scaarifintl@gmail.com>
2023-01-21 14:54:30 +0700

    compare

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# Configures Nginx server with some folders and files
+# Configure Nginx servers, preparing them for web_static deployment	
 apt-get -y update
 apt-get -y install nginx
 service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+mkdir -p data/web_static/releases/test/
+mkdir -p data/web_static/shared/
+echo "Holberton School" > data/web_static/releases/test/index.html
+ln -sf data/web_static/releases/test/ data/web_static/current
+chown -R ubuntu:ubuntu data/
+sed -i '48i\\tlocation /hbnb_static {\n\t\t alias data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
+
+
