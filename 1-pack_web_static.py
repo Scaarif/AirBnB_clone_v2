a914bc5b57b28cca861e3f6e2a94167e3425dfa0
scaarifintl <scaarifintl@gmail.com>
2023-01-21 16:43:55 +0700

    +++

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,14 +15,12 @@ def do_pack():
         # create the directory 'versions' if it doesn't exist
         local('mkdir -p versions')
         # generate the archive into the created folder
-        now = datetime.now()
-        now_fstr = now.strftime("%Y%m%d%H%M%S")
-        archive_path = f'versions/web_static_{now_fstr}.tgz'
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = f'versions/web_static_{now}.tgz'
         # print(f'Packing web_static to {archive_path}')
-        if local(f'tar -cvzf {archive_path} web_static').failed:
-            return None
+        local(f'tar -cvzf {archive_path} web_static/')
         # get the newly created file and create a path to return
-        size = os.path.getsize(archive_path)
+        # size = os.path.getsize(archive_path)
         # print(f'web_static packed: {archive_path} -> {size}Bytes')
         return archive_path
     except Exception:
