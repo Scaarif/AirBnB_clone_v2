edc1bc9bb96809631ffbae6c00796609096a04b3
scaarifintl <scaarifintl@gmail.com>
2023-01-23 12:41:13 +0700

    test

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,53 +1,39 @@
-# Setup web servers for 'web_static' deployment (task 0 using puppet)
-
-# ensure nginx is installed
-exec { 'update apt':
-provider  => shell,
-  command => 'sudo apt-get -y update',
-  before  => Exec['install nginx']
+# Setup the web servers for the deployment of web_static
+exec { '/usr/bin/env apt -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-
-exec { 'install nginx':
-provider  => shell,
-  command => 'sudo apt-get -y install nginx',
-  before  => File['/data/']
+-> file { '/data':
+  ensure  => 'directory'
 }
-
-# create the /data/ folders if they don't already exist (/data/web_static/releases/test/ /shared/)
-file { '/data':
+-> file { '/data/web_static':
   ensure => 'directory'
 }
-file { '/data/web_static':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
 }
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
 }
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/shared':
   ensure => 'directory'
 }
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => 'Holberton School'
+  content => "Holberton School"
 }
-file { '/data/web_static/shared':
-  ensure => 'directory'
-}
-# always create a symbolic link to /data/web_static/releases/test/ folder (/data/web_static/current)
-file { '/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
 }
-# give ownership of the /data/ folder to ubuntu (owner and group)
-exec { 'chown -R ubuntu:ubuntu /data/':
-provider => shell
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
-# update Nginx configuration to serve content of /data/web_static/current to 'hbnb_static' requests (use alias)
 exec { 'add location':
-  command => 'sudo sed -i \'40i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\' /etc/nginx/sites-available/default',
-  before  => Exec['restart nginx']
+  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
+  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
+  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
 }
-# update/restart Nginx after the configuration
-exec { 'restart nginx':
-  command => 'sudo service nginx restart',
+-> service { 'nginx':
+  ensure => running,
 }
