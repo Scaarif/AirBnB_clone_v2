01c198d598fcffda98ceaa95218524ab53e3c6d8
scaarifintl <scaarifintl@gmail.com>
2023-01-26 10:48:52 +0700

    comments

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,20 +10,27 @@ env.hosts = ['54.82.173.163', '18.210.20.118']
 
 def do_deploy(archive_path):
     """
-        Distributes the archive
+        distributes the archive
     """
+    # check that archive_path exists and return False if it doesn't
     if os.path.exists(archive_path):
+        # derive file and folder names from archive name
         filename = archive_path[9:]
         archive_folder = "/data/web_static/releases/" + filename[:-4]
         tmp_archive = "/tmp/" + filename
+        # upload archive
         put(archive_path, "/tmp/")
+        # create necessary repo unzip archive into it
         run("sudo mkdir -p {}".format(archive_folder))
         run("sudo tar -xzf {} -C {}/".format(tmp_archive,
                                              archive_folder))
+        # delete archive file
         run("sudo rm {}".format(tmp_archive))
+        # move unzipped files to correct folder and delete web_static
         run("sudo mv {}/web_static/* {}".format(archive_folder,
                                                 archive_folder))
         run("sudo rm -rf {}/web_static".format(archive_folder))
+        # delete current sym link and craete a new one
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(archive_folder))
 
