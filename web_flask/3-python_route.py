ffbbc22050abecfc6a88dd43dd04429ce60fba3f
scaarifintl <scaarifintl@gmail.com>
2023-02-04 11:56:19 +0300

    task3: implement optional/default variables in (dynamic) URLs

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Starts a Flask application that MUST be lisening on 0.0.0.0, port 5000.
+    Required
+        routes: /:display "Hello HBNB!", /hbnb: display "HBNB" &
+                /c/<text>: display "C" forllowed by 'text' variable (with any
+                underscores replaced by a space)
+                /python/(<text>): display "Python" + what /c/<text> does...
+                with 'text' having the default value "is cool"
+        MUST use the option 'strict_slashes=False in route definition
+"""
+from flask import Flask, escape
+
+# instantiate a Flask application
+app = Flask(__name__)
+app.url_map.strict_slashes = False  # override default globally
+
+
+# define a route to trigger the function defined right after
+@app.route('/')
+def hello_world():
+    """ Returns 'Hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_world_2():
+    """ Returns 'HBNB' """
+    return "HBNB"
+
+
+# the route captures a value (text) from the URL & passes to the function
+@app.route('/c/<text>')
+def hello_world_3(text):
+    """ Returns 'C' followed by (space replaced underscores) text """
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+# route to trigger if no 'text' in url (defaults to defaults value)
+@app.route('/python', defaults={'text': 'is cool'})
+# route to trigger if 'text' in url
+@app.route('/python/<text>')
+def hello_world_4(text):
+    """ Returns 'Python' followed by (space replaced underscores) text
+    Note: <text> defaults to 'is cool' if undefined """
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    # if run as an application (not module), listen on all public IPs
+    app.run(host='0.0.0.0', port=5000)
