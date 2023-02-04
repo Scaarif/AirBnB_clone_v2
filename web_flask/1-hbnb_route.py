5d6cab2e834364574bf5f07f55d0f6ab0830cb78
scaarifintl <scaarifintl@gmail.com>
2023-02-04 10:01:32 +0300

    task 1: make default override (strict_slashes=False) global

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Starts a Flask application that MUST be lisening on 0.0.0.0, port 5000.
+    Required
+        routes: /:display "Hello HBNB!" & /hbnb: display "HBNB"
+        MUST use the option 'strict_slashes=False in route definition
+"""
+from flask import Flask
+
+# instantiate a Flask application
+app = Flask(__name__)
+app.url_map.strict_slashes = False  # override default globally
+
+
+# define a route to trigger the function defined right after
+@app.route('/')
+def hello_world():
+    """ Returns 'Hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_world_2():
+    """ Returns 'HBNB' """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    # if run as an application (not module), listen on all public IPs
+    app.run(host='0.0.0.0', port=5000)
