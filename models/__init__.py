1d461ce879e5d102da2ed5a938dbd689ad1c4fd3
scaarifintl <scaarifintl@gmail.com>
2023-02-05 15:43:20 +0300

    remove print statements from model/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,14 +4,10 @@ import os
 
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE', None)
-db = os.getenv('HBNB_MYSQL_DB', None)
-print(f'storage_type -> {storage_type} & db -> {db}')
 if (storage_type and storage_type == 'db'):
-    print('using db_storage')
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    print('using file_storage')
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
